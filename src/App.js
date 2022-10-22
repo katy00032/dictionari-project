825dc23798ec745ebcb24080269a0244aecc6998
katy00032 <105888000+katy00032@users.noreply.github.com>
2022-10-22 12:14:39 +0200

    1.1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo-small.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+  <header className="App-header">
+  <img src={logo} className="App-logo img-fluid"
+  alt="logo"/>
+  <a href='#' className="btn bth-primary">
+
+  </a>
+  </header>
+    </div>
+  );
+}
+
+export default App;
